1bed4b47b69b210f87970a3915f04f74e1fc338e
Hefri Anesti <hefrykun10@gmail.com>
2024-10-10 19:05:44 +0700

    add console

--- console/app/models.py
+++ console/app/models.py
@@ -25,8 +25,8 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(255), unique=True, nullable=False)
+    username = Column(String(50), nullable=False)
+    password = Column(String(255), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     birth = Column(DateTime, nullable=False)
     address = Column(Text, nullable=True)
